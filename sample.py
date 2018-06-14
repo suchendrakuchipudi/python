ef383c23f2cafe33b7a45e5e8386a84fdd27f76b
Suchendra <suchendrakuchipudi@gmail.com>
2018-06-13 17:50:29 -0700

    second commit

--- sample.py
+++ sample.py
@@ -29,7 +29,12 @@ class TestClass(object):
         print j
 
    
-    
+     def my_second_method():
+        j = []
+        k=10
+        for i in range(1, 100):
+                j.append(i-k)
+        print j
 
     def json_to_csv(self, json_file_path, outfile_path):
         """Convert a file containing a list of flat JSON objects to a csv.
